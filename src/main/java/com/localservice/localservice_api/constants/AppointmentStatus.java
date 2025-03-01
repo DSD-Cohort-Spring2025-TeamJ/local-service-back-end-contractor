0a6c6ce1c2f30967e52c3924f62c58a4eeeba4fe
Joshua Ow <joshuaow@gmail.com>
2025-02-28 20:08:07 -0600

    added lombok, changed name of file to constants, added global exception handlers, added app-dev props, added responseentity in each controller method

--- src/main/java/com/localservice/localservice_api/constants/AppointmentStatus.java
+++
@@ -1,9 +0,0 @@
-package com.localservice.localservice_api.constraints;
-
-public enum AppointmentStatus {
-    PENDING,
-    ASSIGNED,
-    COMPLETED,
-    REJECTED,
-    ACTIVE
-}
--- src/main/java/com/localservice/localservice_api/entity/Service.java
+++ src/main/java/com/localservice/localservice_api/entity/Service.java
@@ -1,11 +1,12 @@
 package com.localservice.localservice_api.entity;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 import java.util.List;
 
+@Data
 @Entity
-@Table(name = "services")
 public class Service {
 
     @Id
@@ -18,36 +19,5 @@ public class Service {
     @OneToMany(mappedBy = "service_id")
     private List<Appointment> appointments;
 
-    public Long getService_id() {
-        return service_id;
-    }
-
-    public void setService_id(Long service_id) {
-        this.service_id = service_id;
-    }
-
-    public String getService_name() {
-        return service_name;
-    }
-
-    public void setService_name(String service_name) {
-        this.service_name = service_name;
-    }
-
-    public int getEstimated_time() {
-        return estimated_time;
-    }
-
-    public void setEstimated_time(int estimated_time) {
-        this.estimated_time = estimated_time;
-    }
-
-    public List<Appointment> getAppointments() {
-        return appointments;
-    }
-
-    public void setAppointments(List<Appointment> appointments) {
-        this.appointments = appointments;
-    }
 }
 
