9488e0b07ba51fe6b6e98b2b4d3d632d3022c2d1
dpala <dpala@mac.attlocal.net>
2025-03-09 17:26:21 -0500

    Crud Operations related to inventory of items.

--- src/main/java/com/localservice/localservice_api/entity/Item.java
+++ src/main/java/com/localservice/localservice_api/entity/Item.java
@@ -11,7 +11,7 @@ import lombok.Data;
 public class Item {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long item_id;
 
     private String item_name;
