9978503a8bed35b0f410c6d65c0203f0f8f2ed23
Dharan kumar pala <42378148+dharan97@users.noreply.github.com>
2025-03-09 20:08:17 -0500

    Crud Operations related to inventory of items. (#40)
    * Crud Operations related to inventory of items.
    
    * Fixes according to the comment.
    
    ---------
    
    Co-authored-by: dpala <dpala@mac.attlocal.net>

---
+++ src/main/java/com/localservice/localservice_api/service/ItemService.java
@@ -0,0 +1,75 @@
+package com.localservice.localservice_api.service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+
+import com.localservice.localservice_api.dto.MultipleUpdateResponseDto;
+import com.localservice.localservice_api.entity.Item;
+import com.localservice.localservice_api.repository.ItemRepository;
+
+import jakarta.persistence.EntityNotFoundException;
+
+@Service
+public class ItemService {
+
+	private final ItemRepository itemRepository;
+
+	public ItemService(ItemRepository itemRepository) {
+		this.itemRepository = itemRepository;
+	}
+
+	// Create a new item
+	public Item createItem(Item item) {
+		return itemRepository.save(item);
+	}
+
+	// Retrieve all items
+	public List<Item> getAllItems() {
+		return itemRepository.findAll();
+	}
+
+	// Retrieve an item by ID
+	public Optional<Item> getItemById(long itemId) {
+		return itemRepository.findById(itemId);
+	}
+
+	// Update an existing item
+	public Item updateItem(long itemId, Item updatedItem) {
+		return getItemById(itemId).map(item -> {
+			item.setItem_name(updatedItem.getItem_name());
+			item.setType(updatedItem.getType());
+			item.setUnit_price(updatedItem.getUnit_price());
+			item.setStock_qty(updatedItem.getStock_qty());
+			return itemRepository.save(item);
+		}).orElseThrow(() -> new EntityNotFoundException("Item not found with id: " + itemId));
+	}
+
+	// Delete an item by ID
+	public void deleteItem(long itemId) {
+		getItemById(itemId)
+				.orElseThrow(() -> new EntityNotFoundException("Item not found with id: " + itemId));
+		itemRepository.deleteById(itemId);
+
+	}
+
+	public MultipleUpdateResponseDto<Item> updateMultipleItems(List<Item> items) {
+		MultipleUpdateResponseDto<Item> res = new MultipleUpdateResponseDto<Item>();
+		items.stream().map(item -> {
+            try {
+                Item existingItem = itemRepository.findById(item.getItem_id())
+                    .orElseThrow(() -> new EntityNotFoundException("Item not found with id: " + item.getItem_id()));
+                updateItem(existingItem.getItem_id(), existingItem);
+                res.getUpdated().add(item);
+                return item.getItem_id();
+            } catch (Exception e) {
+                res.getNotUpdated().add(item);
+                return item.getItem_id();
+            }
+        }).collect(Collectors.toList());
+		return res;
+	}
+
+}
