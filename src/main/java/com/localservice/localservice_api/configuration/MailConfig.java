833d26ffd1c6fc5e43a625059cbf2c4dd109c0db
Sag124 <sagar05091997@gmail.com>
2025-03-09 20:45:36 -0500

    Added an API to update and notify inventory stock (#41)
    Co-authored-by: Raghavendra Sagar <srilekhapulaparthi@Srilekhas-MBP.attlocal.net>
    Co-authored-by: Joshua Ow <joshuaow@gmail.com>

---
+++ src/main/java/com/localservice/localservice_api/configuration/MailConfig.java
@@ -0,0 +1,34 @@
+package com.localservice.localservice_api.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class MailConfig {
+
+    @Value("${spring.mail.username}")
+    private String mailUsername;
+
+    @Value("${spring.mail.password}")
+    private String mailPassword;
+
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.mailtrap.io");
+        mailSender.setPort(2525);
+        mailSender.setUsername(mailUsername);
+        mailSender.setPassword(mailPassword);
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+
+        return mailSender;
+    }
+}
--- src/main/java/com/localservice/localservice_api/repository/ItemRepository.java
+++ src/main/java/com/localservice/localservice_api/repository/ItemRepository.java
@@ -1,11 +1,8 @@
 package com.localservice.localservice_api.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import com.localservice.localservice_api.entity.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-@Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {
 
 }
