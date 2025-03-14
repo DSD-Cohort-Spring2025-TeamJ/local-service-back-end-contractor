0a8caaacb7deb498b5e237f34e1df554d3730b2f
Joshua Ow <joshuaow@gmail.com>
2025-03-13 19:58:19 -0500

    debug ssl

--- src/main/java/com/localservice/localservice_api/configuration/CorsConfig.java
+++ src/main/java/com/localservice/localservice_api/configuration/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "https://thepragmaticplumber.netlify.app")
+                .allowedOrigins("http://localhost:5173", "https://thepragmaticplumber.netlify.app", "https://app.battleoptionsapi.com/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true)
