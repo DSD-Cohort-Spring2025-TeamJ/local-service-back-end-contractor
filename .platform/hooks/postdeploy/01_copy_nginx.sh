a987adebd43323fd83c20b8247578bea8e286a88
Joshua Ow <joshuaow@gmail.com>
2025-03-14 08:48:16 -0500

    debug

--- .platform/hooks/postdeploy/01_copy_nginx.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Ensure Nginx config is in the right location
-cp -f /var/app/current/.platform/nginx/conf.d/https_redirect.conf /var/proxy/staging/nginx/conf.d/ || echo "Nginx Config Not Found"
-
-# Restart Nginx to apply changes
-systemctl restart nginx
