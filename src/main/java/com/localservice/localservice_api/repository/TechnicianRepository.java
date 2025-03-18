0b2cb1ae2610d3d16d6c97434d5e15b8d075f8b4
Joshua Ow <joshuaow@gmail.com>
2025-03-18 12:38:38 -0500

    added functionality to remove reserved time from tech if appointment is rejected

--- src/main/java/com/localservice/localservice_api/repository/TechnicianRepository.java
+++ src/main/java/com/localservice/localservice_api/repository/TechnicianRepository.java
@@ -3,5 +3,8 @@ package com.localservice.localservice_api.repository;
 import com.localservice.localservice_api.entity.Technician;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TechnicianRepository extends JpaRepository<Technician, Long> {
+    Optional<Object> findById(Optional<String> techId);
 }
