e3b5ad68728c977e3b27383d3dd9b7510a2ad360
Joshua Ow <joshuaow@gmail.com>
2025-03-18 12:59:33 -0500

    removed repository call no longer needed, changed the way of querying time from technician

--- src/main/java/com/localservice/localservice_api/repository/TechnicianRepository.java
+++ src/main/java/com/localservice/localservice_api/repository/TechnicianRepository.java
@@ -3,8 +3,5 @@ package com.localservice.localservice_api.repository;
 import com.localservice.localservice_api.entity.Technician;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface TechnicianRepository extends JpaRepository<Technician, Long> {
-    Optional<Object> findById(Optional<String> techId);
 }
