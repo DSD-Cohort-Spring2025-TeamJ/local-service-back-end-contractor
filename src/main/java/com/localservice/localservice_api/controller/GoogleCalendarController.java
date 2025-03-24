d24649c56efb6967fe0f13c8f7b94513dd331216
Joshua Ow <joshuaow@gmail.com>
2025-03-24 10:39:04 -0500

    Oauth (#93)
    * google auth jwt implementation
    
    * remove refreshtoken as non null
    
    * add token in response body instead of url
    
    * fix redirect uri
    
    * try new pop up approach with jwt

--- src/main/java/com/localservice/localservice_api/controller/GoogleCalendarController.java
+++ src/main/java/com/localservice/localservice_api/controller/GoogleCalendarController.java
@@ -4,7 +4,10 @@ import com.google.api.services.calendar.model.Event;
 import com.google.api.services.calendar.model.EventDateTime;
 import com.localservice.localservice_api.dto.EventRequest;
 import com.localservice.localservice_api.service.GoogleCalendarService;
+import com.localservice.localservice_api.service.JwtService;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
@@ -14,26 +17,43 @@ import java.util.List;
 @RequestMapping("/api/calendar")
 public class GoogleCalendarController {
     private final GoogleCalendarService googleCalendarService;
+    private final JwtService jwtService;
 
-    public GoogleCalendarController(GoogleCalendarService googleCalendarService) {
+    public GoogleCalendarController(GoogleCalendarService googleCalendarService, JwtService jwtService) {
         this.googleCalendarService = googleCalendarService;
+        this.jwtService = jwtService;
     }
 
     @GetMapping("/events")
-    public List<Event> getAllEvents(@RequestParam String userId) throws IOException, GeneralSecurityException {
+    public List<Event> getAllEvents(@RequestHeader("Authorization") String authHeader) throws IOException, GeneralSecurityException {
+        String token = authHeader.replace("Bearer ", "");
+
+        if (!jwtService.validateToken(token)) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
+        }
+
+        String userId = jwtService.getUserIdFromToken(token);
         return googleCalendarService.getAllEvents(userId);
     }
 
     @PostMapping("/events")
-    public Event createEvent(@RequestParam String userId, @RequestBody EventRequest eventRequest) throws IOException, GeneralSecurityException {
-        Event event = new Event()
-                .setSummary(eventRequest.getSummary());
+    public Event createEvent(
+            @RequestHeader("Authorization") String authHeader,
+            @RequestBody EventRequest eventRequest
+    ) throws IOException, GeneralSecurityException {
+        String token = authHeader.replace("Bearer ", "");
 
-        // Convert String dateTime to EventDateTime
-        event.setStart(new EventDateTime().setDateTime(eventRequest.getParsedStartTime()));
-        event.setEnd(new EventDateTime().setDateTime(eventRequest.getParsedEndTime()));
+        if (!jwtService.validateToken(token)) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
+        }
+
+        String userId = jwtService.getUserIdFromToken(token);
+
+        Event event = new Event()
+                .setSummary(eventRequest.getSummary())
+                .setStart(new EventDateTime().setDateTime(eventRequest.getParsedStartTime()))
+                .setEnd(new EventDateTime().setDateTime(eventRequest.getParsedEndTime()));
 
         return googleCalendarService.createEvent(userId, event);
-
     }
 }
